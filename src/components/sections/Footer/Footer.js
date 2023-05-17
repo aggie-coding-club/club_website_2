a372c5c11252cccd4b51dc9814e0b2bf4ab8da0d
Carol Geng <98667735+carolgeng@users.noreply.github.com>
2023-05-17 01:15:04 -0500

    change footer  (#9)
    * change footer
    
    Signed-off-by: carolgeng <carolgeng27@gmail.com>
    
    * added linkedin
    
    Signed-off-by: carolgeng <carolgeng27@gmail.com>
    
    * removed unnecessary css
    
    Signed-off-by: carolgeng <carolgeng27@gmail.com>
    
    ---------
    
    Signed-off-by: carolgeng <carolgeng27@gmail.com>

--- src/components/sections/Footer/Footer.js
+++ src/components/sections/Footer/Footer.js
@@ -1,8 +1,43 @@
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import accIcon from "../../../static/icons/acc.png";
+
 export default function Footer() {
+  const flexRow = {
+    display: "flex",
+    direction: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: "1.5rem 0",
+    marginTop: "40px",
+  };
+  const items = {
+    padding: "10px",
+  };
+
+  
+
   return (
-    <div>
-      <p>ACC</p>
-      <p>Github</p>
-    </div>
+    
+
+    <footer>
+      <div style={flexRow}>
+        <img src={accIcon} alt="ACC Icon" width={125} />
+        <div>
+          {/* instagram */}
+          <a href="https://www.instagram.com/aggiecodingclub/?hl=en" target="_blank" style = {items}>
+            <AiFillInstagram size={50}  alt="Instagram Icon" color="3766B4"/>
+          </a>
+
+          <a href="https://www.linkedin.com/company/aggie-codingclub/" target="_blank" style = {items}>
+            <AiFillLinkedin size={50}  alt="Linkedin Icon" color="3766B4"/>
+          </a>
+
+          <a href="https://github.com/aggie-coding-club" target="_blank" style = {items}>
+            <AiFillGithub size={50}  alt="GitHub Icon" color="3766B4"/>
+          </a>
+        </div>
+        
+      </div>
+    </footer>
   );
 }
