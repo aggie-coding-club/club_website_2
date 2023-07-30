a983adeab000e6b84088683d46fc40c726495906
Sofia-Ortega <s.ortega@tamu.edu>
2023-07-30 18:38:46 -0500

    Added discord invite link to discord btn

--- src/components/assets/DiscordButton.js
+++ src/components/assets/DiscordButton.js
@@ -1,4 +1,4 @@
 import { SquareButton } from "./SquareButton";
 export default function DiscordButton() {
-  return <SquareButton variant="contained"> Join Discord</SquareButton>;
+  return <SquareButton variant="contained" href="https://discord.gg/dX8nnXPpra" target="_blank"> Join Discord</SquareButton>;
 }
