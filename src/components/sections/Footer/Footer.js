344c635335c39fc6162c7cd668b105072b7e78b8
Colin <colinlevine7@gmail.com>
2024-07-14 20:51:58 -0700

    Fix footer and add OITs

--- src/components/sections/Footer/Footer.js
+++ src/components/sections/Footer/Footer.js
@@ -1,22 +1,9 @@
-import accIcon from "../../../static/icons/acc.png";
 import Socials from '../../../components/assets/Socials';
 
 export default function Footer() {
-  const flexRow = {
-    display: "flex",
-    direction: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    padding: "1.5rem 0",
-    marginTop: "40px",
-  };
-
   return (
-
-
     <footer>
-      <div style={flexRow}>
-        <img src={accIcon} alt="ACC Icon" width={125} />
+      <div className="flex flex-row justify-center mt-20">
         <Socials size={50} />
 
       </div>
